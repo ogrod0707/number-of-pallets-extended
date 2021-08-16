c113c95dc8b640fd1a9605c2a4ffc75ce01a449d
GimmeSomeCoffee <80617954+ogrod0707@users.noreply.github.com>
2021-08-16 21:45:16 +0200

    added additional trucks to calc

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pallet calculator</title>
     
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -43,11 +43,11 @@ echo <<<END
 
     <label for="pt-width$i">Width&nbsp;</label>
     <input id="pt-width$i"  name="pt-width$i" type="number"><br>
-    <label for="pt-height$i">Height</label>
-    <input id="pt-height$i" name="pt-height$i" type="number"><br>
+    
     <label for="pt-length$i">Length</label>
     <input id="pt-length$i" name="pt-length$i" type="number"><br>
-    
+    <label for="pt-height$i">Height</label>
+    <input id="pt-height$i" name="pt-height$i" type="number"><br>
 
 
     <label for="double-pallet$i">Piętrowalne palety? </label>
@@ -63,7 +63,7 @@ END;
 
 ?> 
 </div>
-<input type="submit" value="Oblicz" >
+<input type="submit" value="Oblicz" class="oblicz">
 
     <br></br>
     </form>
@@ -79,8 +79,6 @@ $container20H = 238;
 $container40L = 1204.5;
 $container40W = 230.9;
 $container40H = 237.9;
-
-
 
 
 if(isset($_POST['pt-width5'])){
@@ -201,10 +199,18 @@ if(isset($_POST['pt-width5'])){
     
       echo"Na kontener 40vd mozna zaladowac: $x palet";
   }
-  else
+  else if($pt_all - 20 <= ($container20L+$container20L))
   {
-      echo"załadunek nie zmiesci sie na jednym kontenerze";
+    echo"Na dwa kontenery 20vd mozna zaladowac: <br>$x palet ";
   }
+  else if($pt_all- 20 <= ($container20L + $container40L))
+  {
+    echo"Potrzeba kontenera 20vd oraz 40vd żeby zaladowac: $x palet";
+  }
+else
+{
+  echo"Sprawdź wymiary lub oblicz samodzielnie :)";
+}
 }
 
 
@@ -307,10 +313,18 @@ else if(isset($_POST['pt-width4'])){
     
       echo"Na kontener 40vd mozna zaladowac: $x palet";
   }
-  else
+  else if($pt_all - 20 <= ($container20L+$container20L))
   {
-      echo"załadunek nie zmiesci sie na jednym kontenerze";
+    echo"Na dwa kontenery 20vd mozna zaladowac: <br>$x palet ";
   }
+  else if($pt_all- 20 <= ($container20L + $container40L))
+  {
+    echo"Potrzeba kontenera 20vd oraz 40vd żeby zaladowac: $x palet";
+  }
+else
+{
+  echo"Sprawdź wymiary lub oblicz samodzielnie :)";
+}
 }
 
 
@@ -393,122 +407,148 @@ else if(isset($_POST['pt-width3'])){
       
         echo"Na kontener 40vd mozna zaladowac: $x palet";
     }
-    else
-    {
-        echo"załadunek nie zmiesci sie na jednym kontenerze";
-    }
+    else if($pt_all - 20 <= ($container20L+$container20L))
+      {
+        echo"Na dwa kontenery 20vd mozna zaladowac: <br>$x palet ";
+      }
+      else if($pt_all- 20 <= ($container20L + $container40L))
+      {
+        echo"Potrzeba kontenera 20vd oraz 40vd żeby zaladowac: $x palet";
+      }
+  else
+  {
+      echo"Sprawdź wymiary lub oblicz samodzielnie :)";
+  }
 }
+
 
 
 else if(isset($_POST['pt-width2'])){
  
-    $pt_length1 = $_POST['pt-length1'];
-    $pt_width1 = $_POST['pt-width1'];
-    $pt_height1 = $_POST['pt-height1'];
-    $pt_number1 = $_POST['pt-number1'];
-    $pt_length2 = $_POST['pt-length2'];
-    $pt_width2 = $_POST['pt-width2'];
-    $pt_height2 = $_POST['pt-height2'];
-    $pt_number2 = $_POST['pt-number2'];
-    // $pt_double2 = $_POST['double-pallet2'];
-    $x = $pt_number1+$pt_number2;
-    $pt_amount1 =$pt_length1 * $pt_number1;
-    $pt_amount2 =$pt_length2 * $pt_number2;
+  $pt_length1 = $_POST['pt-length1'];
+  $pt_width1 = $_POST['pt-width1'];
+  $pt_height1 = $_POST['pt-height1'];
+  $pt_number1 = $_POST['pt-number1'];
+  $pt_length2 = $_POST['pt-length2'];
+  $pt_width2 = $_POST['pt-width2'];
+  $pt_height2 = $_POST['pt-height2'];
+  $pt_number2 = $_POST['pt-number2'];
+  // $pt_double2 = $_POST['double-pallet2'];
+  $x = $pt_number1+$pt_number2;
+  $pt_amount1 =$pt_length1 * $pt_number1;
+  $pt_amount2 =$pt_length2 * $pt_number2;
+  
+ 
+  if($pt_width1*2-10 < $container20W){ // sprawdza czy mozna ustawic dwa rzędy
+         
+      $pt_amount1 /= 2;
+    }
+
     
+    if(isset($_POST['double-pallet1'])){ // sprawdza czy uzytkownik chce pietrowalne palety i czy mozna
    
-    if($pt_width1*2-10 < $container20W){ // sprawdza czy mozna ustawic dwa rzędy
-           
+      if($pt_height1 * 2  - 15 < $container20H) {
         $pt_amount1 /= 2;
       }
-
-      
-      if(isset($_POST['double-pallet1'])){ // sprawdza czy uzytkownik chce pietrowalne palety i czy mozna
-     
-        if($pt_height1 * 2  - 15 < $container20H) {
-          $pt_amount1 /= 2;
-        }
-        else{
-          echo"Nie mozna piętrować palety 1 rodzaju<br>";
-        }
-       
-        
+      else{
+        echo"Nie mozna piętrować palety 1 rodzaju<br>";
       }
+     
+      
+    }
 
 
-      if($pt_width2*2-10 < $container20W){ // sprawdza czy mozna ustawic dwa rzędy
-           
+    if($pt_width2*2-10 < $container20W){ // sprawdza czy mozna ustawic dwa rzędy
+         
+      $pt_amount2 /= 2;
+    }
+    
+    if(isset($_POST['double-pallet2']) ){// sprawdza czy uzytkownik chce pietrowalne palety i czy mozna
+      
+      if($pt_height2 * 2  - 15 < $container20H) {
         $pt_amount2 /= 2;
       }
-      
-      if(isset($_POST['double-pallet2']) ){// sprawdza czy uzytkownik chce pietrowalne palety i czy mozna
-        
-        if($pt_height2 * 2  - 15 < $container20H) {
-          $pt_amount2 /= 2;
-        }
-        else{
-          echo"Nie mozna piętrować palety 2 rodzaju<br>";
-        }
-
+      else{
+        echo"Nie mozna piętrować palety 2 rodzaju<br>";
       }
 
-      $pt_all = $pt_amount1 + $pt_amount2;
-    if($pt_all - 20 <= $container20L) {
+    }
 
-      echo"Na kontener 20vd mozna zaladowac: $x palet";
-        
-    }
-    else if($pt_all - 20 <= $container40L){
-       
+    $pt_all = $pt_amount1 + $pt_amount2;
+  if($pt_all - 20 <= $container20L) {
+
+    echo"Na kontener 20vd mozna zaladowac: $x palet";
       
-        echo"Na kontener 40vd mozna zaladowac: $x palet";
-    }
-    else
-    {
-        echo"załadunek nie zmiesci sie na jednym kontenerze";
-    }
+  }
+  else if($pt_all - 20 <= $container40L){
+     
+    
+      echo"Na kontener 40vd mozna zaladowac: $x palet";
+  }
+  else if($pt_all - 20 <= ($container20L+$container20L))
+      {
+        echo"Na dwa kontenery 20vd mozna zaladowac: <br>$x palet ";
+      }
+      else if($pt_all- 20 <= ($container20L + $container40L))
+      {
+        echo"Potrzeba kontenera 20vd oraz 40vd żeby zaladowac: $x palet";
+      }
+  else
+  {
+      echo"Sprawdź wymiary lub oblicz samodzielnie :)";
+  }
 }
 
 
 else if(isset($_POST['pt-width1'])){
 
-        $pt_length1 = $_POST['pt-length1'];
-        $pt_width1 = $_POST['pt-width1'];
-        $pt_height1 = $_POST['pt-height1'];
-        $pt_number1 = $_POST['pt-number1'];
-        $pt_amount1 =$pt_length1 * $pt_number1; // ile palety zajmą długości kontenera
-        
-        if($pt_width1*2-10 < $container20W){
-           
+      $pt_length1 = $_POST['pt-length1'];
+      $pt_width1 = $_POST['pt-width1'];
+      $pt_height1 = $_POST['pt-height1'];
+      $pt_number1 = $_POST['pt-number1'];
+      $pt_amount1 =$pt_length1 * $pt_number1; // ile palety zajmą długości kontenera
+      
+      if($pt_width1*2-10 < $container20W){
+         
+        $pt_amount1 /= 2;
+      }
+      
+      if(isset($_POST['double-pallet1'])){
+
+        if($pt_height1 * 2  - 15 < $container20H) {
           $pt_amount1 /= 2;
         }
-        
-        if(isset($_POST['double-pallet1'])){
-
-          if($pt_height1 * 2  - 15 < $container20H) {
-            $pt_amount1 /= 2;
-          }
-          else{
-            echo"Nie mozna piętrować palet<br>";
-          }
-  
-
+        else{
+          echo"Nie mozna piętrować palet<br>";
         }
-       
-        if($pt_amount1 - 20 <= $container20L) {
-            
-                echo"Na kontener 20vd mozna zaladowac: <br>$pt_number1 palet ";
-            
-        }
-        else if($pt_amount1 - 20 <= $container40L){
-           
+
+
+      }
+     
+      if($pt_amount1 - 20 <= $container20L) {
           
-            echo"Na kontener 40vd mozna zaladowac: $pt_number1 palet";
-        }
-        else
-        {
-            echo"załadunek nie zmiesci sie na jednym kontenerze";
-        }
-    }
+              echo"Na kontener 20vd mozna zaladowac: <br>$pt_number1 palet ";
+          
+      }
+      else if($pt_amount1 - 20 <= $container40L){
+         
+        
+          echo"Na kontener 40vd mozna zaladowac: $pt_number1 palet";
+      }
+      else if($pt_amount1 - 20 <= ($container20L+$container20L))
+      {
+        echo"Na dwa kontenery 20vd mozna zaladowac: <br>$pt_number1 palet ";
+      }
+      else if($pt_amount1 - 20 <= ($container20L + $container40L))
+      {
+        echo"Potrzeba kontenera 20vd oraz 40vd żeby zaladowac: $pt_number1 palet";
+      }
+      else
+      {
+          echo"Sprawdź wymiary lub oblicz samodzielnie :)";
+      }
+  }
+
 
 
 
